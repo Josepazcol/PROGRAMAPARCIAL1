4322dd82f30ecac818f71909ca26fc7174b9ef90
josepablopazcolindres <elpavoenpanizado@gmail.com>
2023-03-04 12:18:35 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese un año: ");
+        int year = Convert.ToInt32(Console.ReadLine());
+
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+        {
+            Console.WriteLine("{0} es un año bisiesto.", year);
+        }
+        else
+        {
+            Console.WriteLine("{0} no es un año bisiesto.", year);
+        }
+    }
+}
